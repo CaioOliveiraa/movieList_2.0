9376c556341d4dd7fe565543716f2387953c0972
unisanta <unisanta@santacecilia.br>
2024-09-25 16:57:46 -0300

    add diretorio movie-list e services

---
+++ movie-list/src/services/api.ts
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+});
+
+export const registerUser = async (name: string, password: string) => {
+    try {
+        const response = await api.post('/auth/register', { name, password });
+        return response.data;
+    } catch (error) {
+        throw new Error('Erro ao cadastrar usuásrio');
+    }
+};
+
+export const loginUser = async (name: string, password: string) => {
+    try {
+        const response = await api.post('/auth/login', { name, password });
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+        return token;
+    } catch (error) {
+        throw new Error('Erro ao fazer login');
+    }
+};
+
+export const addMovie = async (title: string, description: string, type: string) => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await api.post(
+            '/movies/add',
+            { title, description, type },
+            {
+                headers: {
+                    Authorization: `Beaver ${token}`
+                }
+            }
+        );
+        return response.data;
+
+    } catch (error) {
+        throw new Error('Erro ao cadastrar mídia')
+    }
+};
+
+export const getMovies = async () => {
+    try {
+        const token = localStorage.getItem('token');
+        const response = await api.get('/movies', {
+            headers: {
+                Authorization: `Beaver ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error('Erro ao buscar mídias')
+    }
+};
