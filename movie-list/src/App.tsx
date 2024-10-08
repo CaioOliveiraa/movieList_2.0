29a49e6db1777ea7ad09e6bbe8e49a2e718825a7
Caio Oliveira <caio2601@gmail.com>
2024-10-08 18:16:54 -0300

    fix: Retirando auth para /home

--- movie-list/src/App.tsx
+++ movie-list/src/App.tsx
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
 import MoviePage from './pages/moviePage';
-import HomePage from './pages/homePage'
-import AddPage from './pages/addPage'
+import HomePage from './pages/homePage';
+import AddPage from './pages/addPage';
 
 import './App.css';
 
 const App: React.FC = () => {
-  const isAuthenticated = localStorage.getItem('token');
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('token'));
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route
-            path='/home'
-            element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />} />
-          <Route
-            path="/movies"
-            element={isAuthenticated ? <MoviePage /> : <Navigate to="/login" />}
-          />
-          <Route
-            path='/addMovie'
-            element={isAuthenticated ? <AddPage /> : <Navigate to="/login" />}
-          />
-
-
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/movies" element={isAuthenticated ? <MoviePage /> : <Navigate to="/login" />} />
+          <Route path="/addMovie" element={isAuthenticated ? <AddPage /> : <Navigate to="/login" />} />
+          <Route path="*" element={isAuthenticated ? <HomePage /> : <Navigate to='/login' />} />
         </Routes>
       </div>
     </Router>
--- movie-list/src/pages/login.tsx
+++ movie-list/src/pages/login.tsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { loginUser } from '../services/api';
 import AuthForm from '../components/authForm';
 import { useNavigate } from 'react-router-dom';
@@ -8,10 +7,18 @@ const LoginPage: React.FC = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
+    // Verificar se o token já existe e redirecionar imediatamente para '/home' se estiver autenticado
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            navigate('/home', { replace: true });  
+        }
+    }, [navigate]);
+
     const handleLogin = async (name: string, password: string) => {
         try {
-            await loginUser(name, password);
-            navigate('/home')
+            await loginUser(name, password);  
+            navigate('/home', { replace: true });  
         } catch (error) {
             setErrorMessage('Falha ao fazer login. Verifique seus dados.');
         }
