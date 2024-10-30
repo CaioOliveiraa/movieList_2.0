ec210c40bac64d351d6b3d0718fb15c7b31e8244
Caio Oliveira <caio2601@gmail.com>
2024-10-30 18:00:03 -0300

    feat: updateMovie e deleteMovie api

--- movie-list-api/src/controller/movieContoller.ts
+++ movie-list-api/src/controller/movieContoller.ts
@@ -35,3 +35,57 @@ export const addMovie = async (req: AuthenticatedRequest, res: Response) => {
         return res.status(400).json({ message: 'Erro ao adicionar filme' });
     }
 };
+
+export const updateMovie = async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.userId;
+    const movieId = req.params.movieId;
+    const { title, description, type } = req.body;
+
+    if (!userId) {
+        return res.status(401).json({ message: 'Usuário não autenticado' });
+    }
+
+    try {
+        const movie = await Movie.findOne({ _id: movieId, userId });
+
+        if (!movie) {
+            return res.status(404).json({ message: 'Filme não encontrado' });
+        }
+
+        movie.title = title || movie.title;
+        movie.description = description || movie.description;
+        movie.type = type || movie.type;
+
+        const updatedMovie = await movie.save();
+        console.log(`Filme atualizado: ${updatedMovie._id}`);
+        return res.status(200).json(updatedMovie);
+
+    } catch (error) {
+        console.error('Erro ao editar filme:', error);
+        return res.status(500).json({ message: 'Erro ao editar filme' });
+    }
+};
+
+export const deleteMovie = async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.userId;
+    const movieId = req.params.movieId;
+
+    if (!userId) {
+        return res.status(401).json({ message: 'Usuário não autenticado' });
+    }
+
+    try {
+        const movie = await Movie.findOneAndDelete({ _id: movieId, userId });
+
+        if (!movie) {
+            return res.status(404).json({ message: 'Filme não encontrado' });
+        }
+
+        console.log(`Filme excluído: ${movie._id}`);
+        return res.status(200).json({ message: 'Filme excluído com sucesso' });
+
+    } catch (error) {
+        console.error('Erro ao excluir filme:', error);
+        return res.status(500).json({ message: 'Erro ao excluir filme' });
+    }
+};
--- movie-list-api/src/routes/movieRoutes.ts
+++ movie-list-api/src/routes/movieRoutes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getMovies, addMovie } from '../controller/movieContoller';
+import { getMovies, addMovie, updateMovie, deleteMovie } from '../controller/movieContoller';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.get('/movies', authMiddleware, getMovies);
 router.post('/movies/add', authMiddleware, addMovie);
+router.put('/movies/:movieId', authMiddleware, updateMovie);
+router.delete('/movies/:movieId', authMiddleware, deleteMovie);
 
 export default router;
